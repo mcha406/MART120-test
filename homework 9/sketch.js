c295dec7de41dcc83c0122d9f633fc6d0edb8e36
mcha406 <gb211419@umconnect.umt.edu>
2026-03-14 18:27:44 -0600

    shapes into me html

---
+++ homework 9/sketch.js
@@ -0,0 +1,39 @@
+function setup()
+ {
+    createCanvas(500, 400);
+}
+
+    function draw()
+ {
+    background('#DFEB63');
+   fill ('rgb(238,211,201)')
+   circle (250,115,100)
+   fill ('#94CBF8')
+   rect (223,165,55,135);
+   
+   stroke('rgb(200,88,13)');
+    strokeWeight(6);
+   line(225,70,120, 170);
+   line (370,180,280,70)
+
+   point(250, 125);
+   
+   
+   circle(220,110,20)
+   circle(277,110,20)
+            
+   fill ('#F17070')
+   triangle(280,135,240,150,220,135)
+
+   fill('rgb(238,211,201)')
+   rect(205,170,20,80)
+   rect(275,170,20,80)
+
+   fill('black')
+   rect(230,265,20,100)
+   rect(250,265,20,100)
+
+   textSize(20);
+   text('"first try"', 10, 30);
+   text('by: gwen brenna', 340, 380)
+ }
